54be0f1242ab7b221ed8d81f6d8a400eb9ccf82d
HooferDevelops <BatHun0214A@mischools.org>
2022-01-26 16:48:30 -0500

    table pretty print!

--- public/backend/lua-loader.js
+++ public/backend/lua-loader.js
@@ -34,9 +34,59 @@ class LuaLoader{
         makeGlobalFunctionList(L, "util", {
             "execute": (L) => {
                 let script = lua.lua_tojsstring(L, 1);
+                lauxlib.luaL_checktype(L, 1, lua.LUA_TSTRING);
 
                 let result = this.executeScript(script);
                 lua.lua_pushboolean(L, result);
+
+                return 1;
+            },
+            "tableprint": (L) => {
+                let table = lua.lua_settop(L, 1);
+                lauxlib.luaL_checktype(L, 1, lua.LUA_TTABLE);
+
+                
+                let print = (L, i=0) => {
+                    let result = {}
+
+                    lua.lua_pushvalue(L, -1-i);
+
+                    lua.lua_pushnil(L);
+
+                    while (lua.lua_next(L, -2) != 0) {
+                        lua.lua_pushvalue(L, -2);
+
+                        if (lua.lua_type(L, -2) == lua.LUA_TSTRING) {
+                            let key = lua.lua_tojsstring(L, -1);
+                            let value = lua.lua_tojsstring(L, -2);
+                            result[key] = value;
+                        } else if (lua.lua_type(L, -2) == lua.LUA_TNUMBER) {
+                            let key = lua.lua_tojsstring(L, -1);
+                            let value = lua.lua_tonumber(L, -2);
+                            result[key] = value;
+                        } else if (lua.lua_type(L, -2) == lua.LUA_TBOOLEAN) {
+                            let key = lua.lua_tojsstring(L, -1);
+                            let value = lua.lua_toboolean(L, -2);
+                            result[key] = value;
+                        } else if (lua.lua_type(L, -2) == lua.LUA_TTABLE) {
+                            let key = lua.lua_tojsstring(L, -1);
+                            let value = print(L, 1);
+
+                            result[key] = value;
+                        } else if (lua.lua_type(L, -2) == lua.LUA_TFUNCTION) {
+                            let key = lua.lua_tojsstring(L, -1);
+                            result[key] = "function";
+                        }
+                        
+                        lua.lua_pop(L, 2);
+                    }
+
+                    lua.lua_pop(L, 1);
+
+                    return result;
+                }
+
+                console.log(print(L));
 
                 return 1;
             }
@@ -63,7 +113,7 @@ class LuaLoader{
                 const file = await request("GET", "/lua/" + path);
 
                 // log it to console
-                console.log("loaded " + path);
+                console.warn("loaded " + path);
                 
                 _this.data.push({
                     name: name, path: path, type: type, file: file
--- public/lua/test.lua
+++ public/lua/test.lua
@@ -1,10 +1,27 @@
-for _,x in pairs(game) do
-
-end
-
 print("Hello from lua! :p")
-
-print(game)
 
 print("LUA:", "attempting to load: test2")
 print("LUA:", util.execute("test2"))
+
+util.tableprint({
+    "Hello",
+    "World",
+    "Lua",
+    "is",
+    "cool",
+    false,
+    x1 = 5,
+    x2 = {
+        "z",
+        {
+            "lul"
+        }
+    },
+    x3 = {
+        5
+    }
+})
+
+util.tableprint(game)
+util.tableprint(util)
+
